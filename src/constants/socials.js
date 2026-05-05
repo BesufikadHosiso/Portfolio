9a24c5ae518ed1f1f614df583e7fa9b215b271b9
BesuHosiso <besuyeboss@gmail.com>
2026-05-05 10:37:28 +0300

    feat: add real social media links

--- src/constants/socials.js
+++ src/constants/socials.js
@@ -1,22 +1,22 @@
 export const socials = [
     {
         name: 'GitHub',
-        url: 'https://github.com/besufikad17',
+        url: 'https://github.com/BesuHosiso',
         icon: 'BiLogoGithub',
     },
     {
         name: 'LinkedIn',
-        url: 'https://linkedin.com/in/besufikad',
+        url: 'https://www.linkedin.com/in/besufikad-hosiso-23a2313aa/',
         icon: 'BiLogoLinkedinSquare',
     },
     {
         name: 'YouTube',
-        url: 'https://youtube.com/@besufikad',
+        url: 'https://youtube.com/@besu-hosiso',
         icon: 'BiLogoYoutube',
     },
     {
         name: 'TikTok',
-        url: 'https://tiktok.com/@besufikad',
+        url: 'https://www.tiktok.com/@besu_hosiso1',
         icon: 'BiLogoTiktok',
     },
 ];
